6f2b4ec501770bcf60032cafc793545c21a33044
Francisco Flores <francisco.flores@crowdint.com>
2015-08-04 19:42:19 -0500

    modifica el ejercicio de clase_alumno 04/08/15

--- Clase_alumno.rb
+++ Clase_alumno.rb
@@ -1,11 +1,12 @@
 #Script para crear una clase, sus metodos y objetos
 #Se emplea set y get para asignar y obtener los valores
 class Alumno
-        def initialize(name = "", last_name = "", id = 0)
+        def initialize(name = "", last_name = "", id = 0, birthday = "01/01/1990")
             @name = name
             @last_name = last_name
             @id = id
             @clave = ""
+            @birthday = birthday
         end
 
         #Metodos de Set
@@ -21,6 +22,10 @@ class Alumno
             @id = id
         end
 
+        def set_birthday(birthday)
+            @birthday = birthday
+        end
+
         #Metodos de Get
         def get_name
             return @name
@@ -32,6 +37,10 @@ class Alumno
 
         def get_id
             return @id
+        end
+
+        def get_birthday
+            return @birthday
         end
 
 #Split separa @name donde hay espacio y lo guarda en cadena como un nuevo array
@@ -46,7 +55,7 @@ class Alumno
                 cadena = values[0]
                 @clave = cadena[0]
                 cadena2 = values[1]
-                @clave = @clave + cadena2
+                @clave = @clave + cadena2[0]
             end
             values2 = @last_name.split(" ")
             if values2.count == 1
@@ -54,22 +63,28 @@ class Alumno
                 @clave = @clave + cadena3[0..1]
             else
                 cadena3 = values2[0]
-                @clave = cadena3[0]
+                @clave = @clave + cadena3[0]
                 cadena4 = values2[1]
-                @clave = @clave + cadena4
+                @clave = @clave + cadena4[0]
             end
+            values3 = @birthday.split("/")
+                @clave = @clave + values3[0] + values3[1] + values3[2][2..3] + "-" + @id.to_s
+                #cadena5 = values3[2]
+                #@clave = @clave + cadena5[2..3]
             @clave.upcase!
-        end
+      end
 end
 #Creacion de un nuevo alumno haciendo uso del set
 alumnito = Alumno.new
-alumnito.set_name("Cisco")
-alumnito.set_last_name("Achaques")
-alumnito.set_id(007)
+alumnito.set_name("Jose Francisco")
+alumnito.set_last_name("Flores nada")
+alumnito.set_id(107)
+alumnito.set_birthday("01/01/1950")
 
 #Creacion de un nuevo alumno con valores
-alumnita = Alumno.new("Lola","Contreras",03)
+alumnita = Alumno.new("Lola","Contreras",03,"01/24/1988")
 
 puts "El nombre del alumno es #{alumnito.get_name} #{alumnito.get_last_name} con no. de control #{alumnito.get_id}"
 puts "El nombre del alumno es #{alumnita.get_name} #{alumnita.get_last_name} con no. de control #{alumnita.get_id}"
 puts "La clave del alumno #{alumnito.get_name} es #{alumnito.clave}"
+puts "La clave del alumno #{alumnita.get_name} es #{alumnita.clave}"
